9d93c9c7c8ca822190e701430a9632e1a8f96ebe
DiegoCard97 <citrusdonjose@gmail.com>
2025-12-29 07:51:24 -0300

    app.py

--- app.py
+++ app.py
@@ -1,10 +1,39 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, session
 
 app = Flask(__name__)
+app.secret_key = "clave_secreta_simple"
 
-@app.route("/")
-def inicio():
-    return "Hola Diego, Render funciona 🚀"
+# Usuarios de prueba
+USUARIOS = {
+    "admin": "1234",
+    "juan": "abcd",
+    "ana": "9999"
+}
+
+@app.route("/", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        usuario = request.form["usuario"]
+        password = request.form["password"]
+
+        if usuario in USUARIOS and USUARIOS[usuario] == password:
+            session["usuario"] = usuario
+            return redirect("/panel")
+        else:
+            return render_template("login.html", error="Usuario o contraseña incorrectos")
+
+    return render_template("login.html")
+
+@app.route("/panel")
+def panel():
+    if "usuario" not in session:
+        return redirect("/")
+    return render_template("panel.html", usuario=session["usuario"])
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect("/")
 
 if __name__ == "__main__":
     app.run()
